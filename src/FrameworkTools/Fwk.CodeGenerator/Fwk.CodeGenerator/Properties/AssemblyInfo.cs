b9df9a05d0696b8f926c40350767ef4e5e0fc0f4
marcelo.oviedo <marcelo.oviedo@8dc54c6a-2c21-11de-acd2-81e04a9d018a>
2011-08-01 15:43:46 +0000

    Migration to visual studio 2010 and .net framework 4.0

--- src/FrameworkTools/Fwk.CodeGenerator/Fwk.CodeGenerator/Properties/AssemblyInfo.cs
+++ src/FrameworkTools/Fwk.CodeGenerator/Fwk.CodeGenerator/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
 
