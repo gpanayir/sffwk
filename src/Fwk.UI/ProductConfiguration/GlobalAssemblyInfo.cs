881f3a63a1c93d617cb1e77bbe80a863489a88d1
marcelo.oviedo <marcelo.oviedo@8dc54c6a-2c21-11de-acd2-81e04a9d018a>
2012-01-23 12:08:24 +0000

    delette fwk.ui.common project

--- src/Fwk.UI/ProductConfiguration/GlobalAssemblyInfo.cs
+++ src/Fwk.UI/ProductConfiguration/GlobalAssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("10.0.0.0")]
-[assembly: AssemblyFileVersion("10.0.0.9")]
+[assembly: AssemblyFileVersion("10.0.0.10")]
